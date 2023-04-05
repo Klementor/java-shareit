1b39c687dd1ca9110408fd474e858b571bc20a5d
Klementor <mihailkrylov8189@gmail.com>
2023-04-05 12:18:36 +0300

    2

--- server/src/main/java/ru/practicum/shareit/ShareItServer.java
+++ server/src/main/java/ru/practicum/shareit/ShareItServer.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShareItApp {
+public class ShareItServer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShareItApp.class, args);
+		SpringApplication.run(ShareItServer.class, args);
 	}
 
 }
